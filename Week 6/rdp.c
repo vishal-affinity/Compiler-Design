36fd98c61726bdee98da748ff8f45b560994732b
Vishal Kumar Singh <103824619+vishal-affinity@users.noreply.github.com>
2022-11-10 11:41:04 +0530

    Update rdp.c

--- Week 6/rdp.c
+++ Week 6/rdp.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-int S(),L(),Ldash();
+int E(),Edash(),T(),Tdash(),F();
 char *ip;
 char string[50];
 int main()
@@ -10,98 +10,120 @@ scanf("%s",string);
 ip=string;
 printf("\n\nInput\tAction\n--------------------------------\n");
 
-if(S() && *ip=='\0')
-{
+if(E() && ip==’\0’){
 printf("\n--------------------------------\n");
 printf("\n String is successfully parsed\n");
 }
-else
-{
+else{
 printf("\n--------------------------------\n");
 printf("Error in parsing String\n");
 }
+}
+int E()
+{
+printf("%s\tE->TE' \n",ip);
+if(T())
+{
+if(Edash())
+{
+return 1;
+}
+else
 return 0;
 }
-int S()
-{
-    
-    if(*ip=='(')
-    {
-        ip++;
-        printf(" %s\tS->(L) \n",ip);
-        if(L())
-        {
-            if(*ip==')')
-            {
-                ip++;
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else if(*ip=='a')
-    {
-        ip++;
-        printf("%s\tS->a \n",ip);
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+else
+return 0;
 }
-int L()
+int Edash()
 {
- printf("%s\tL->SL' \n",ip);   
-    if(S())
-    {
-        if(Ldash())
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else
-    {
-        return 0;
-    }
+if(*ip=='+')
+{
+printf("%s\tE'->+TE' \n",ip);
+ip++;
+if(T())
+{
+if(Edash())
+{
+return 1;
 }
-int Ldash()
+else
+return 0;
+}
+else
+return 0;
+}
+else
 {
-    
-    if(*ip==',')
-    {
-        ip++;
-        printf("%s\tL'->,SL' \n",ip);
-        if(S())
-        {
-            if(Ldash())
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else
-    {
-        printf("%s\tL'->E\n",ip);
-        return 1;
-    }
+printf("%s\tE'->^ \n",ip);
+return 1;
+}
+}
+int T()
+{
+printf("%s\tT->FT' \n",ip);
+if(F())
+{
+
+if(Tdash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+int Tdash()
+{
+if(*ip=='*')
+{
+printf("%s\tT'->*FT' \n",ip);
+ip++;
+if(F())
+{
+if(Tdash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+else
+{
+printf("%s\tT'->^ \n",ip);
+return 1;
+}
+}
+int F()
+{
+if(*ip=='(')
+{
+printf("%s\tF->(E) \n",ip);
+ip++;
+if(E())
+{
+if(*ip==')')
+{
+ip++;
+return 0;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+
+else if(*ip=='i')
+{
+ip++;
+printf("%s\tF->id \n",ip);
+return 1;
+}
+else
+return 0;
 }
